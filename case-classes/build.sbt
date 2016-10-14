07e8fa123a2de6e5615b2493b61d8756837d9836
Naomi Jacobs <naomi@mavenlink.com>
2016-10-14 15:30:07 -0700

    Case classes

--- case-classes/build.sbt
+++ case-classes/build.sbt
@@ -4,6 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-sbtVersion := "0.13.12"
+sbtVersion := "0.13.11"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test"
--- case-classes/src/main/scala/CaseClasses.scala
+++ case-classes/src/main/scala/CaseClasses.scala
@@ -4,15 +4,26 @@ case class Location(city: String, country: String)
 case class Person(name: String, location: Location)
 
 object CaseClasses {
-  def extract_name(peep: Person): String = {
-    ""
+  def extract_name(peep: Person): String = peep match{
+    case Person(name, _) => name
+  }
+
+  def is_from_country(country: String)(peep: Person): Boolean = peep match {
+    case Person(_, Location(_, `country`)) => true
+    case _ => false
   }
 
   def is_canadian(peep: Person): Boolean = {
-    false
+    return is_from_country("Canada")(peep);
+  }
+
+  def filter(people: List[Person], test: Person => Boolean): List[Person] = people match {
+    case head :: tail if test(head) => head :: filter(tail, test)
+    case head :: tail => filter(tail, test)
+    case _ => Nil
   }
 
   def names_from(people: List[Person], country: String): List[String] = {
-    List.empty[String]
+    filter(people, is_from_country("USA")).map(_.name)
   }
 }
